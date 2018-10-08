3a1a57bf2ae6961969e9af84c287f504a4965af8
wwt-5 <40057831+wwt-5@users.noreply.github.com>
2018-10-08 23:43:52 +0800

    Update news.py

--- sina/news.py
+++ sina/news.py
@@ -1,5 +1,6 @@
 import requests
 from lxml import etree
+import csv
 
 url = 'https://news.sina.com.cn/'
 response = requests.get(url)
@@ -7,7 +8,14 @@ response.encoding = 'utf-8'
 html = response.text
 s = etree.HTML(html)
 file = s.xpath('//*[@id="blk_gjxw_011"]/li/a')
-for i in file:
-    title = i.xpath("./text()")[0]
-    url = i.xpath('./@href')[0]
-    print("{}      {}".format(title,url))
+
+
+with open('data.csv','w',encoding='utf-8')as csvfile:
+    writer = csv.writer(csvfile)
+    for i in file:
+        title = i.xpath('./text()')[0]
+        url = i.xpath('./@href')[0]
+        writer.writerow([title,url])
+
+
+
