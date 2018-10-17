50fee90e183c863ed999d76741c06b3beb321b34
wwt-5 <40057831+wwt-5@users.noreply.github.com>
2018-10-17 08:02:50 +0800

    Update test.py

--- Selenium-use/CSUST/test.py
+++ Selenium-use/CSUST/test.py
@@ -1,1 +1,15 @@
+from selenium import webdriver
+import time
 
+browser = webdriver.Chrome()
+url = 'http://10.64.6.5/'
+browser.get(url)
+button1 = browser.find_element_by_id('m14')
+button1.click()
+time.sleep(1)
+win2 = browser.current_window_handle
+index = browser.switch_to.frame('frmHomeShow')  #iframe嵌套的切入当前页面
+input1 = browser.find_element_by_id('txt_asmcdefsddsd')
+input1.send_keys('学号')
+input2 = browser.find_element_by_id('txt_pewerwedsdfsdff')
+input2.send_keys('密码')
